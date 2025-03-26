3288f2ec0787da6ae593fbf4befd75ada2e6357d
abril <abriltc04@gmail.com>
2025-03-26 19:18:35 +0100

    Receptes model done

--- web/models.py
+++ web/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Receptes(models.Model):
+    id_recipe = models.AutoField(primary_key=True)
+    name_recipe = models.CharField(max_length=120)
+    description_recipe = models.TextField()
+    instructions_recipe = models.TextField()
+    image_recipe = models.ImageField(upload_to='recipe_images')
+    source = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name_recipe
