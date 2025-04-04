92a7ac2c955f98423ac08548337c2d8e7865d583
Marc <marcferreresudl@gmail.com>
2025-04-04 12:18:28 +0200

    recipe model done

--- web/models.py
+++ web/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     def __str__(self):
         return self.name
 
-class Ingredients (models.Model):
+class Ingredient (models.Model):
     ingredient_id = models.AutoField(primary_key=True)
     ingredient_name = models.CharField(max_length=100, unique=True)
     ingredient_amount = models.IntegerField(max_length=10)
@@ -20,4 +20,12 @@ class Ingredients (models.Model):
     def __str__(self):
         return self.ingredient_name
 
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient)
+    instructions = models.TextField()
+    image_url = models.URLField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
