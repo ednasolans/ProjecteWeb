978bc2383e4d59811ab452eb0286aa34a600eca9
Marc <marcferreresudl@gmail.com>
2025-04-04 13:03:15 +0200

    proves

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web.apps.WebConfig',
-    'accounts',
 ]
 
 MIDDLEWARE = [
--- web/apps.py
+++ web/apps.py
@@ -5,8 +5,5 @@ class WebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'web'
 
-class AccountsConfig(AppConfig):
-    name = 'accounts'
-
     def ready(self):
-        import accounts.signals
+        import web.signals
