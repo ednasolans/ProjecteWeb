96a7c0b11418f0a2ce4dff12334f18be35d648a6
Marc <marcferreresudl@gmail.com>
2025-04-28 12:15:11 +0200

    dependency added + admin panel updated

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Ingredient, Recipe
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
